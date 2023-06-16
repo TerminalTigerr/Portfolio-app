94c17fd86dda95c4579aace6c0dfa5e44d14de10
BitHero <nuhumusah874@gmail.com>
2023-06-16 00:21:56 +0000

    Contact section + EmailJS

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -39,3 +39,27 @@ function blurHeader() {
     }
 }
 window.addEventListener('scroll', blurHeader)
+
+/*================= Email JS ============*/
+const contactForm = document.getElementById('contact-form')
+const contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_zsf8ygq', 'template_xdmoabx', '#contact-form', 'vd11_vSYF8KVM9vFK')
+        .then(() => {
+            // Show sent message
+            contactMessage.textContent = 'Message sent successfully ✅'
+
+            // Remove message after 5 seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+        }, () => {
+            // Show error message
+            contactMessage.textContent = 'Message not sent (service error) ❌'
+        })
+    }
+contactForm.addEventListener('submit', sendEmail)
